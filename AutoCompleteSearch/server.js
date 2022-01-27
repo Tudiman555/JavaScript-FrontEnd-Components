b3f5227f97660420994e717415f6e3f31f0584b6
Tushar-Agarwal <agarwaltushar35@gmail.com>
2022-01-27 23:14:58 +0530

    styling-added

--- AutoCompleteSearch/server.js
+++ AutoCompleteSearch/server.js
@@ -1,6 +1,6 @@
 function getRandomString({ length }) {
   const characterChoices =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let characters = [];
   while (characters.length < length) {
     const randomIndex = getRandomInteger({
@@ -42,7 +42,7 @@ function generateSuggestion({ prefix }) {
 
 function autoCompleteHandler(data) {
   const MAX_PREFIX_LENGTH = 10;
-  const RATIO_AUXILLERY = 0.1; // 10%
+  const RATIO_AUXILIARY = 0.1; // 10%
   const RESULT_LENGTH = 10;
   let results = [];
   if (data.length > MAX_PREFIX_LENGTH) {
@@ -56,17 +56,17 @@ function autoCompleteHandler(data) {
       continue;
     }
 
-    if (Math.random() < RATIO_AUXILLERY_DATA) {
+    if (Math.random() < RATIO_AUXILIARY) {
       for (let i = 0; i < 2; i++) {
         results.push({
           suggestion,
-          auxillery: getRandomString({
+          auxiliary: getRandomString({
             length: getRandomInteger({ min: 5, max: 15 }),
           }),
         });
       }
     } else {
-      results.push({ suggestion, auxillery: "" });
+      results.push({ suggestion, auxiliary: "" });
     }
   }
   return results;
